38a6317e069ed9e2e788cce90654d374bb6e3f28
Pravallika Sai Krothapalli <pravallika.app@gmail.com>
2021-12-12 19:23:43 -0600

    upload

---
+++ Mobile/Mobile_ICP14/Source/Notification/app/src/main/java/com/example/notification/AlertDetails.java
@@ -0,0 +1,15 @@
+
+package com.example.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlertDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_details);
+    }
+}
